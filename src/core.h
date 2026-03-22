bdd061cb2a7508cc11ed0105b29d2732575eb4b4
EmomaxD <emreleno@gmail.com>
2026-03-22 10:51:44 +0000

    add depth_buffer, texture, mesh structs and vec3 math helpers to core.h

--- src/core.h
+++ src/core.h
@@ -1,6 +1,8 @@
 #ifndef CORE_H
 #define CORE_H
 
+#include <math.h>
+
 typedef char unsigned u8;
 typedef short unsigned u16;
 typedef int unsigned u32;
@@ -41,8 +43,8 @@ struct image_u32
 };
 
 typedef struct {
-    float x; // X coordinate
-    float y; // Y coordinate
+    float x;
+    float y;
 } vec2;
 
 typedef struct {
@@ -56,5 +58,69 @@ typedef struct {
 typedef struct {
     float m[4][4];
 } mat4;
+
+/* Depth buffer for z-buffering */
+struct depth_buffer
+{
+    u32 Width;
+    u32 Height;
+    float *Data;
+};
+
+/* Texture image */
+struct texture
+{
+    u32 Width;
+    u32 Height;
+    u32 *Pixels;
+};
+
+/* 3D vertex with position, texture coordinates, and normal */
+struct vertex3d
+{
+    vec3 position;
+    vec2 texcoord;
+    vec3 normal;
+};
+
+/* Triangle defined by 3 vertex indices */
+struct triangle3d
+{
+    int v[3];
+};
+
+/* Simple dynamic array for OBJ loading */
+struct mesh
+{
+    vertex3d *vertices;
+    int vertexCount;
+    triangle3d *triangles;
+    int triangleCount;
+};
+
+/* vec3 helpers */
+static inline vec3 Vec3Sub(vec3 a, vec3 b) {
+    return {a.x - b.x, a.y - b.y, a.z - b.z};
+}
+static inline vec3 Vec3Add(vec3 a, vec3 b) {
+    return {a.x + b.x, a.y + b.y, a.z + b.z};
+}
+static inline vec3 Vec3Scale(vec3 v, float s) {
+    return {v.x * s, v.y * s, v.z * s};
+}
+static inline float Vec3Dot(vec3 a, vec3 b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+static inline vec3 Vec3Cross(vec3 a, vec3 b) {
+    return {a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x};
+}
+static inline float Vec3Length(vec3 v) {
+    return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+static inline vec3 Vec3Normalize(vec3 v) {
+    float len = Vec3Length(v);
+    if (len > 0.0001f) return Vec3Scale(v, 1.0f / len);
+    return {0, 0, 0};
+}
 
 #endif // CORE_H
